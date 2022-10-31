6881f18bbc22189291a7de1c2f9fef8681b7791a
Clarkxielf <71970105+Clarkxielf@users.noreply.github.com>
2022-10-31 16:46:14 +0800

    Add files via upload

---
+++ datasets/ReuseKnnDPNoise.py
@@ -0,0 +1,114 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from datasets.KNNGraph import KNNGraph
+from datasets.AuxFunction import FFT
+from tqdm import tqdm
+import pickle
+# -------------------------------------------------------------
+signal_size = 1024
+
+# label
+label = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+         2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+         3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+         4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
+         4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
+
+# generate Training Dataset and Testing Dataset
+def get_files(sample_length, root, InputType, task, test=False):
+    '''
+    This function is used to generate the final training set and test set.
+    root:The location of the data set
+    normalname:List of normal data
+    dataname:List of failure data
+    '''
+    file = ['baseline','1st','2nd','3rd','9']
+
+    Subdir = []
+    for i in file:
+        sub_root = os.path.join(root, i)
+        file_name = os.listdir(sub_root)
+        for j in file_name:
+            Subdir.append(os.path.join(sub_root, j))
+
+    data = []
+
+    for i in tqdm(range(len(Subdir))):
+
+        data1 = data_load(sample_length, Subdir[i],label=label[i],InputType=InputType,task=task)
+        data += data1
+
+    return data
+
+
+def data_load(signal_size, root, label, InputType, task):
+    '''
+    This function is mainly used to generate test data and training data.
+    root:Data location
+    '''
+
+    fl = pd.read_csv(root,sep='\t',usecols=[1],header=None,encoding='ANSI')
+    fl.drop(fl.head(23).index, inplace=True)
+    fl = np.array(fl, dtype=np.float64)
+    fl = (fl - fl.min()) / (fl.max() - fl.min())
+    fl = fl.reshape(-1,)
+    data = []
+    start, end = 0, signal_size
+    while end <= fl[:signal_size*1024].shape[0]:
+        if InputType == "TD":
+            x = fl[start:end]
+        elif InputType == "FD":
+            x = fl[start:end]
+            x = wgn(x, 20)
+            x = FFT(x)
+        else:
+            print("The InputType is wrong!!")
+
+        data.append(x)
+        start += signal_size
+        end += signal_size
+
+    graphset = KNNGraph(10,data,label,task)
+
+    return graphset
+
+
+class ReuseKnnDPNoise(object):
+    num_classes = 5
+
+    def __init__(self, sample_length, data_dir, InputType, task):
+        self.sample_length = sample_length
+        self.data_dir = data_dir
+        self.InputType = InputType
+        self.task = task
+
+
+
+    def data_preprare(self, test=False):
+        if len(os.path.basename(self.data_dir).split('.')) == 2:
+            with open(self.data_dir, 'rb') as fo:
+                list_data = pickle.load(fo, encoding='bytes')
+        else:
+            list_data = get_files(self.sample_length, self.data_dir, self.InputType, self.task, test)
+            with open(os.path.join(self.data_dir, "ReuseKnnDPNoise.pkl"), 'wb') as fo:
+                pickle.dump(list_data, fo)
+
+        if test:
+            test_dataset = list_data
+            return test_dataset
+        else:
+
+            train_dataset, val_dataset = train_test_split(list_data, test_size=0.20, random_state=40)
+
+            return train_dataset, val_dataset
+
+def wgn(x, snr):
+    Ps = np.sum(abs(x)**2)/len(x)
+    Pn = Ps/(10**((snr/10)))
+    noise = np.random.randn(len(x)) * np.sqrt(Pn)
+    signal_add_noise = x + noise
+    return signal_add_noise
---
+++ datasets/__init__.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+from datasets.CWRUKnn import CWRUKnn
+from datasets.CWRURadius import CWRURadius
+from datasets.CWRUPath import CWRUPath
+
+
+from datasets.MFPTKnn import MFPTKnn
+from datasets.MFPTRadius import MFPTRadius
+from datasets.MFPTPath import MFPTPath
+
+
+from datasets.PUKnn import PUKnn
+from datasets.PURadius import PURadius
+from datasets.PUPath import PUPath
+
+
+from datasets.SEUKnn import SEUKnn
+from datasets.SEURadius import SEURadius
+from datasets.SEUPath import SEUPath
+
+
+from datasets.XJTUSpurgearKnn import XJTUSpurgearKnn
+from datasets.XJTUSpurgearRadius import XJTUSpurgearRadius
+from datasets.XJTUSpurgearPath import XJTUSpurgearPath
+
+
+from datasets.XJTUGearboxKnn import XJTUGearboxKnn
+from datasets.XJTUGearboxRadius import XJTUGearboxRadius
+from datasets.XJTUGearboxPath import XJTUGearboxPath
+
+from datasets.CMAPSS_graph import CMAPSS_graph
+from datasets.PHM2010_graph import PHM2010_graph
+
+from datasets.SELF_BearingKnn import SELF_BearingKnn
+from datasets.SELF_BearingPath import SELF_BearingPath
+from datasets.SELF_BearingRadius import SELF_BearingRadius
+
+from datasets.ReuseKnnAE import ReuseKnnAE
+from datasets.ReuseKnnDP import ReuseKnnDP
+from datasets.ReusePathAE import ReusePathAE
+from datasets.ReusePathDP import ReusePathDP
+from datasets.ReuseRadiusAE import ReuseRadiusAE
+from datasets.ReuseRadiusDP import ReuseRadiusDP
+from datasets.ReuseKnnDPNoise import ReuseKnnDPNoise
